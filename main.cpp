0f28ee2bab3586a0b2936cddb301dcdb2c72ac81
dxhInter <dxh0530@gmail.com>
2024-02-18 19:30:05 -0500

    Fix timing calculation

--- main.cpp
+++ main.cpp
@@ -21,10 +21,10 @@ using namespace std::chrono_literals;
 
 // Mark as volatile to enable thread safety.
 volatile bool btn_mode_pressed = false;
-volatile bool is_startup = true;
-size_t blink_frequency;
-metronome metro;
-std::vector<size_t> bpm_list;
+volatile bool is_startup = true; // The startup state of the LED false for on, ture for off.
+size_t blink_frequency; // The frequency of the LED blinking.
+metronome metro; // The metronome object.
+std::vector<size_t> bpm_list; // The list of BPMs.
 
 // Run an additional loop separate from the main one.
 void blink() {
@@ -37,6 +37,7 @@ void blink() {
 		// Perform the blink if we are pressed,
 		// otherwise just set it to off.
 		if (!metro.is_timing() && !is_startup){
+			// If the metronome is not running, the LED will blink.
 			gpioWrite(LED_GREEN,PI_HIGH);
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 			gpioWrite(LED_GREEN,PI_LOW);
@@ -44,11 +45,11 @@ void blink() {
 	}
 }
 
-static void check_tap_button () {
+// Check the tap button if it is pressed
+static void blink_led_when_tap () {
 	gpioWrite(LED_RED,PI_HIGH);
 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	gpioWrite(LED_RED,PI_LOW);
-
 }
 
 /**
@@ -65,26 +66,19 @@ void msg_reply(web::http::http_request msg, size_t content, int mode) {
 	if(mode == 1){
     	jsonResponse[U("bpm")] = web::json::value::number(content);
 		response.set_body(jsonResponse);
-	}
-	// }else if(mode == 2){
-	// 	web::json::value jsonResponse;
-	// 	web::json::value json_bpm_list = web::json::value::array();
-    //     // 如果 bpm_list 不为空，则填充 JSON 数组
-    //     if (!bpm_list.empty()) {
-    //         for (size_t i = 0; i < bpm_list.size(); ++i) {
-    //         	json_bpm_list[i] = web::json::value::number(bpm_list[i]);
-    //     	}
-    //     }
-    //     jsonResponse[U("bpm_list")] = json_bpm_list;
-	// 	response.set_body(jsonResponse);
-	// }
-	else{
+	} else {
     	jsonResponse[U("msg")] = web::json::value::string("your request has been processed successfully!");
 		response.set_body(jsonResponse);
 	}
 	msg.reply(response);
 }
 
+/**
+ * @brief Override the msg.reply to send the error reply message
+ * @param msg the http request
+ * @param content the content to be sent
+ * @param status_code the http status code to be sent
+*/
 void msg_wrong_reply(web::http::http_request msg, std::string content, web::http::status_code status_code){
 	web::http::http_response response(status_code);
 	response.headers().set_content_type(U("application/json"));
@@ -95,18 +89,24 @@ void msg_wrong_reply(web::http::http_request msg, std::string content, web::http
 	msg.reply(response);
 }
 
-
+/**
+ * @brief Update the blink frequency
+*/
 void updateBPM() {
 	blink_frequency = metro.get_bpm();
 	if(blink_frequency != 0){
+		// The frequency of the LED blinking is 60 seconds divided by the BPM.
 		blink_frequency = (60*1000) / blink_frequency;
+		// The LED will blink at the frequency of the BPM.
 		is_startup = false;
 	} else{
 		is_startup = true;	
 	}
 }
 
-
+/**
+ * @brief The REST service to get the BPM
+*/
 void getBPM(web::http::http_request msg) {
 	try{
 		msg_reply(msg, metro.get_bpm(), 1);
@@ -116,6 +116,9 @@ void getBPM(web::http::http_request msg) {
 	}
 }
 
+/**
+ * @brief The REST service to get the BPM list
+*/
 void getBPMlist(web::http::http_request msg) {
 	bpm_list = metro.get_bpm_list();
 	try{
@@ -138,6 +141,7 @@ void getBPMlist(web::http::http_request msg) {
 	}
 }
 
+// The REST service to get the MIN and MAX BPM
 void getMIN(web::http::http_request msg) {
 	msg_reply(msg, metro.getMIN_MAX(MIN), 1);
 }
@@ -145,23 +149,27 @@ void getMAX(web::http::http_request msg) {
 	msg_reply(msg, metro.getMIN_MAX(MAX), 1);
 }
 
+// The REST service to delete the MIN and MAX BPM
 void delMIN(web::http::http_request msg) {
 	metro.delMIN_MAX(MIN);
 	updateBPM();
 	msg_reply(msg, 0 ,0);
 }
-
 void delMAX(web::http::http_request msg) {
 	metro.delMIN_MAX(MAX);
 	updateBPM();
 	msg_reply(msg, 0 ,0);
 }
 
+/**
+ * @brief The REST service to set the BPM
+ * @param msg the http request
+*/
 void setBPM(web::http::http_request msg){
 	msg.extract_json().then([=](web::json::value req_body){
 		try{
 			auto bpmValue = req_body[U("bpm")].as_integer();
-            metro.addNewBPM(bpmValue);
+            metro.addBPM(bpmValue);
 			updateBPM();
             msg_reply(msg, 0, 0);
 		}catch(const std::exception& e){
@@ -172,6 +180,10 @@ void setBPM(web::http::http_request msg){
 	}).wait();
 }
 
+/**
+ * @brief The REST service to delete the BPM in the BPM list, url is /bpm/list
+ * @param msg the http request
+*/
 void deleteBPM(web::http::http_request msg){
 	msg.extract_json().then([=](web::json::value req_body){
 		try{
@@ -179,27 +191,22 @@ void deleteBPM(web::http::http_request msg){
 			bool isSuccess = metro.deleteBeatByValue(bpmValue);
 			updateBPM();
 			if (isSuccess == false){
-				// msg.reply(400, U("Bad Request, please check the bpm value"));
 				msg_wrong_reply(msg, "Bad Request, please check the bpm value", web::http::status_codes::BadRequest);
 			}else {
 				msg_reply(msg, 0, 0);
 			}
 		}catch(const std::exception& e){
-			// msg.reply(400, U("Bad Request, please check the bpm value"));
 			msg_wrong_reply(msg, "Bad Request, please check the bpm value", web::http::status_codes::BadRequest);
 			std::cerr << e.what() << std::endl;
 		}
 	});
 }
 
-// This program shows an example of input/output with GPIO, along with
-// a dummy REST service.
-// ** You will have to replace this with your metronome logic, but the
-//    structure of this program is very relevant to what you need. **
+
 int main() {
-	// WiringPi must be initialized at the very start.
 	// This setup method uses the Broadcom pin numbers. These are the
 	// larger numbers like 17, 24, etc, not the 0-16 virtual ones.
+	// we are using the pigpio library to control the GPIO pins.
 	if (gpioInitialise() < 0) {
         std::cerr << "pigpio initialization failed." << std::endl;
         return 1;
@@ -211,16 +218,16 @@ int main() {
 	gpioSetMode(LED_GREEN, PI_OUTPUT);
     gpioSetMode(BTN_MODE, PI_INPUT);
 	gpioSetMode(BTN_TAP, PI_INPUT);
-    gpioSetPullUpDown(BTN_MODE, PI_PUD_UP);
-	gpioSetPullUpDown(BTN_TAP, PI_PUD_UP);
 	// Note that you can also set a pull-down here for the button,
 	// if you do not want to use the physical resistor.
-	//pullUpDnControl(BTN_MODE, PUD_DOWN);
+	// we are using the pull-up resistor.
+    gpioSetPullUpDown(BTN_MODE, PI_PUD_UP);
+	gpioSetPullUpDown(BTN_TAP, PI_PUD_UP);
+
+
 
 	// Configure the rest services after setting up the pins,
 	// but before we start using them.
-	// ** You should replace these with the BPM endpoints. **
-
 	auto getBPM_rest = rest::make_endpoint("/bpm");
 	auto getBPMLIST_rest = rest::make_endpoint("/bpm/list");
 	auto getMIN_rest = rest::make_endpoint("/bpm/min");
@@ -271,24 +278,21 @@ int main() {
 			}
 			std::this_thread::sleep_for(std::chrono::milliseconds(200));
 		}
-		
-		
+
 		if(metro.is_timing()){
 			if(gpioRead(BTN_TAP) == PI_LOW){
+				// the tap button is pressed
 				std::cout << "Tap button pressed" << std::endl;
 				metro.tap();
-				check_tap_button();
+				//blink the red led when the tap button is pressed
+				blink_led_when_tap();
 				std::this_thread::sleep_for(std::chrono::milliseconds(200));
 			}
 		}
 		
 		std::this_thread::sleep_for(10ms);
-
-		// ** Note that the above is for testing when the button
-		// is held down. For detecting "taps", or momentary
-		// pushes, you need to check for a "rising edge" -
-		// this is when the button changes from LOW to HIGH... **
 	}
+	// Clean up the GPIO pins.
 	gpioTerminate();
 	return 0;
 }
--- metronome.hpp
+++ metronome.hpp
@@ -24,7 +24,8 @@ public:
 	void start_timing(){
 		std::cout << "Begin timing"<< std::endl;
 		m_timing = true;
-		firstTap = lastTap = 0;
+		//reset the first and last tap time
+		first_tap_time = last_tap_time = 0;
 		m_beat_count = 0;
 	}
 	// Call when leaving "learn" mode
@@ -39,10 +40,10 @@ public:
 				m_beats[i] = m_beats[i+1];
 			}			
 			//calculate seconds between the first and last tap
-			double seconds = ((double)(lastTap - firstTap) / (double) 1000);
+			double seconds = ((double)(last_tap_time - first_tap_time) / (double) 1000);
 			if(seconds <= 0){
 				// if the time is less than 0, then the BPM is 0
-				std::cerr << "Something is wrong with the time calculation"<< lastTap << firstTap << seconds << std::endl;
+				std::cerr << "Something is wrong with the time calculation"<< last_tap_time << first_tap_time << seconds << std::endl;
 				m_beats[beat_samples-1] = 0;
 			} else{
 				// calculate the BPM with minutes
@@ -69,11 +70,11 @@ public:
 	// Insert the time at the next free position of m_beats
 	void tap(){
 		size_t time_stamp = currentMillis();
-		if(firstTap == 0){
+		if(first_tap_time == 0){
 			//first tap to start the timing
-			firstTap = time_stamp;
+			first_tap_time = time_stamp;
 		}
-		lastTap = time_stamp;
+		last_tap_time = time_stamp;
 		m_beat_count++;
 		std::cout<< "beat count: " << m_beat_count << std::endl;
 		std::cout<< "m_beats list=[ ";
@@ -93,6 +94,9 @@ public:
 		return m_beats[beat_samples-1];
 		
 	}
+	/**
+	 * @brief Get the BPM list
+	*/
 	std::vector<size_t> get_bpm_list() const {
 		std::vector<size_t> bpm_list;
 		for(int i = 0 ; i < beat_samples ; i++){
@@ -101,46 +105,58 @@ public:
 		return bpm_list;
 	}
 
+	/**
+ 	 * @brief Get the MIN or MAX BPM
+	 * @param mode 0 for get MIN, mode 1 for get MAX 
+	*/
 	size_t getMIN_MAX(int mode) const {
-		size_t ans;
-		ans = m_beats[0];
+		size_t res;
+		res = m_beats[0];
 		//mode 0 for get MIN, mode 1 for get MAX
 		if (mode == 0) {
 			for(int i = 1 ; i < beat_samples ; i++){
-				if(m_beats[i] < ans) {
-					ans = m_beats[i];
+				if(m_beats[i] < res) {
+					res = m_beats[i];
 				}
 			}
-			return ans;
+			return res;
 		} else {
 			for(int i = 1 ; i < beat_samples ; i++){
-				if(m_beats[i] > ans) {
-					ans = m_beats[i];
+				if(m_beats[i] > res) {
+					res = m_beats[i];
 				}
 			}
-			return ans;
+			return res;
 		}
 		return 0;
 	}
 
+	/**
+	 * @brief Delete the MIN or MAX BPM
+	 * @param mode 0 for delete MIN, mode 1 for delete MAX 
+	*/
 	void delMIN_MAX(int mode) {
 		size_t ans = getMIN_MAX(mode);
 		for (int i = 0; i< beat_samples ; i++){
 			if(m_beats[i] == ans){
-				// TODO delete the value
 				m_beats[i] = 0;
 			}
 		}
 	}
 
-	//replace the new BPM with the oldest one
-	void addNewBPM(size_t val) {
+	/**
+	 * @brief Add the new BPM value to the BPM list
+	*/
+	void addBPM(size_t new_bpm) {
 		for(int i = 0 ; i < beat_samples-1 ; i++) {
 			m_beats[i] = m_beats[i+1];
 		}
-		m_beats[beat_samples-1] = val;
+		m_beats[beat_samples-1] = new_bpm;
 	}
 
+    /**
+	 * @brief Delete the BPM value from the BPM list
+	*/
 	bool deleteBeatByValue(size_t value) {
 		if (value == 0) {
 			return true;
@@ -159,8 +175,8 @@ public:
 
 private:
 	bool m_timing;
-	size_t firstTap;
-	size_t lastTap;
+	size_t first_tap_time;
+	size_t last_tap_time;
 	// Insert new samples at the end of the array, removing the oldest
 	size_t m_beats[beat_samples];
 	size_t m_beat_count;
